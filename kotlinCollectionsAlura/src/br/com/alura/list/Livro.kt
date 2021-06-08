3112c36cb5949780d7db31b97bf09a9f573c2705
eugabiviana <gabi.viana88@gmail.com>
2021-06-08 18:56:38 -0300

    Projeto desenvolvido no curso de Kotlin: coleçoes, arrays e listas

---
+++ kotlinCollectionsAlura/src/br/com/alura/list/Livro.kt
@@ -0,0 +1,16 @@
+package br.com.alura.list
+
+data class Livro (
+    val titulo: String,
+    val autor: String,
+    val anoPublicacao: Long,
+    val editora: String? = null
+): Comparable<Livro> {
+    override fun compareTo(other: Livro): Int {
+        return this.anoPublicacao.compareTo(other.anoPublicacao)
+    }
+}
+
+/*
+Esse tipo de classe 'data' já traz funções prontas como o toString. Assim, não precisamos criar manualmente;
+ */
